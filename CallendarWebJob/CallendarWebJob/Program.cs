7cd467e5e5e5a002f92aabbb1c3e4ed08ed05cb9
Ofri Harlev <Ofri.harlev@gmail.com>
2015-11-21 05:49:15 -0800

    Initial Commit

---
+++ CallendarWebJob/CallendarWebJob/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace CallendarWebJob
+{
+    class Program
+    {
+
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://callendarjs.azure-mobile.net/","oVQZgOmoEbRwJDmYflgvIRsTQJYvBj27");
+        static void Main(string[] args)
+        {
+            test();
+        }
+
+
+         static void test()
+        {
+            CallTable item = new CallTable();
+            item.Id = "1";
+            item.PhoneNumber = "415-469-7636";
+            item.timeToExcute =DateTime.Now;
+            item.Text = "testing";
+            item.complete = false;
+            MobileService.GetTable<CallTable>().InsertAsync(item).Wait();
+        }
+
+
+
+        
+    }
+}
---
+++ callendarService/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace callendarService.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
