c29ae24b3384ff973fb0789e93e283cab37750b4
Ofri Harlev <Ofri.harlev@gmail.com>
2015-11-22 08:02:39 -0800

    Revert "confrincing at current state"
    This reverts commit b10e7539f238d977611a092c0d6061de820360d2.

--- CallendarWebJob/CallendarWebJob/ConferenceTable.cs
+++ CallendarWebJob/CallendarWebJob/ConferenceTable.cs
@@ -12,7 +12,6 @@ namespace CallendarWebJob
         public string Id { get; set; }
         public string ConferenceId { get; set; }
         public string Text { get; set; }
-        public string TextMessage { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime timeToExcute { get; set; }
         public bool complete { get; set; }
